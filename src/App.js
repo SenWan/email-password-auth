f0619c562567af3290894ca26c8fd886fd8a99ed
U Sen Wan <senwan96@gmail.com>
2022-04-07 19:13:14 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,127 @@
-import './App.css';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form'
+import './App.css';
+import app from "./firebase.init";
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
-import app from './firebase.init';
-import { useState } from 'react';
+import { useState } from "react";
 
-const auth = getAuth(app);
+const auth = getAuth(app)
 
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
+  const [error, setError] = useState('');
   const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleEmailChange = e => {
-   setEmail(e.target.value); 
+  const handleEmailBlur = event => {
+    setEmail(event.target.value);
   }
 
-  const handlePasswordChange = e => {
-    setPass(e.target.value)
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value);
   }
 
-  const handleFormSubmit = e => {
-    createUserWithEmailAndPassword(auth, email, pass)
-    .then(resutl => {
-      const user = resutl.user;
-      console.log(user);
+  const handleRegisteredChange = event =>{
+    setRegistered(event.target.checked)
+  }
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Password Should contain at least one special character');
+      return;
+    }
+    setValidated(true);
+    setError('');
+
+    if(registered) {
+      console.log(email, password);
+      signInWithEmailAndPassword(auth, email, password)
+      .then(result =>{
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error =>{
+        console.error(error);
+        setError(error.message);
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+        setEmail('');
+        setPassword('');
+        verifyEmail();
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message);
+      })
+    }
+
+    
+    event.preventDefault();
+  }
+
+  const handlePasswordReset = () =>{
+    sendPasswordResetEmail(auth, email)
+    .then(() =>{
+      console.log('email sent')
     })
-    .catch(error => {
-      console.error(error)
-    })
-    e.preventDefault();
   }
+
+  const verifyEmail = () =>{
+    sendEmailVerification(auth.currentUser)
+    .then(() =>{
+      console.log('Email Verification Sent');
+    })
+  }
+
   return (
     <div>
-      <Form onSubmit={handleFormSubmit} className='container mt-5'>
-        <h2 className='text-primary text-center fw-bold'>Please Register</h2>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control onBlur={handleEmailChange} type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+      <div className="registration w-50 mx-auto mt-5">
+        <h2 className="text-primary">Please { registered ? 'Login': 'Register'}!!</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control onBlur={handlePasswordChange} type="password" placeholder="Password" />
-        </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-  </div>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered?" />
+          </Form.Group>
+          <p className="text-danger">{error}</p>
+          <Button onClick={handlePasswordReset} variant="link">Forget Password?</Button>
+          <br />
+          <Button variant="primary" type="submit">
+            {registered ? 'Login':  'Register'}
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
 
